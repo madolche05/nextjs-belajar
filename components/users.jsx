9e06602d7df7eeb7f19263f58718ab600028fa8c
madolche <govania05@gmail.com>
2022-09-22 01:03:47 +0700

    fix api

--- components/users.jsx
+++ components/users.jsx
@@ -1,7 +1,7 @@
 import Getpost from "../libs/Api";
 export default function Users() {
 const data=new Getpost
-const {result,isloading,iserror}=data.getData()
+const {result,isloading,iserror}=data.getData('https://developzero.000webhostapp.com/api/api')
 if(iserror) return <div>Error..</div>
 if(isloading) return <div>Loading..</div>
 return (
